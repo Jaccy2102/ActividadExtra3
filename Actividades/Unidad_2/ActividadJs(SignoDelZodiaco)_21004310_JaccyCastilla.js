466f9aeab552ab201ab42c5c088f719190bda628
Jaccy Castilla <jaccy.castilla@galileo.edu>
2024-02-04 19:15:53 -0600

    second commit

---
+++ Actividades/Unidad_2/ActividadJs(SignoDelZodiaco)_21004310_JaccyCastilla.js
@@ -0,0 +1,67 @@
+  
+  var mes = 1;
+  var dia = 1;
+  
+  mes = prompt ("Ingrese el número de su mes de nacimiento")
+  dia = prompt ("Ingrese su día de nacimiento")
+
+  if (mes == 1 && dia <=20) {
+    alert ("Su signo es Capricornio");
+  } else if (mes == 1 && dia >=21<=31) {
+    alert ("Su signo es Acuario")
+  }
+if (mes == 2 && dia <=19) {
+    alert ("Su signo es Acuario");
+  } else if (mes == 2 && dia >=20<=29) {
+    alert ("Su signo es Piscis")
+  }
+if (mes == 3 && dia <=20) {
+    alert ("Su signo es Piscis");
+  } else if (mes == 3 && dia >=21<=31) {
+    alert ("Su signo es Aries")
+  }
+if (mes == 4 && dia <=20) {
+    alert ("Su signo es Aries");
+  } else if (mes == 4 && dia >=21<=30) {
+    alert ("Su signo es Tauro")
+  }
+if (mes == 5 && dia <=20) {
+    alert ("Su signo es Tauro");
+  } else if (mes == 5 && dia >=21<=31) {
+    alert ("Su signo es Géminis")
+  }
+if (mes == 6 && dia <=20) {
+    alert ("Su signo es Géminis");
+  } else if (mes == 6 && dia >=21<=30) {
+    alert ("Su signo es Cáncer")
+  }
+if (mes == 7 && dia <=22) {
+    alert ("Su signo es Cáncer");
+  } else if (mes == 7 && dia >=23<=31) {
+    alert ("Su signo es Leo")
+  }
+if (mes == 8 && dia <=23) {
+    alert ("Su signo es Leo");
+  } else if (mes == 8 && dia >=24<=31) {
+    alert ("Su signo es Virgo")
+  }
+if (mes == 9 && dia <=22) {
+    alert ("Su signo es Virgo");
+  } else if (mes == 9 && dia >=23<=30) {
+    alert ("Su signo es Libra")
+  }
+if (mes == 10 && dia <=23) {
+    alert ("Su signo es Libra");
+  } else if (mes == 10 && dia >=24<=31) {
+    alert ("Su signo es Escorpio")
+  }
+if (mes == 11 && dia <=22) {
+    alert ("Su signo es Escorpio");
+  } else if (mes == 11 && dia >=23<=30) {
+    alert ("Su signo es Sagitario")
+  }
+if (mes == 12 && dia <=21) {
+    alert ("Su signo es Sagitario");
+  } else if (mes == 12 && dia >=22<=31) {
+    alert ("Su signo es Capricornio")
+  }
